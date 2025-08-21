c8264f21d437a2e1ebfe721bb9f3e8b4878eef10
Andrew Nguyen <andrewnguyen@utexas.edu>
2025-08-21 17:30:08 -0500

    Added fastfetch/neofetch to startup and chose a new theme

--- config.fish
+++ config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set fish_cursor_default block
+
+    set fish_greeting ""
+
+    # Run neofetch or fastfetch whenever I open a new terminal emulator window
+    # so that everyone knows that I use Arch, btw
+    # * Is not run while inside tmux
+    if status --is-interactive; and not set -q TMUX
+        if type -q fastfetch
+            fastfetch
+        else if type -q neofetch
+            neofetch
+        end
+    end
+
 end
