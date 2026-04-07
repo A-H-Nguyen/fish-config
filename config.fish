3c56214bcd8fac7eef757ca6a6011eaecaab586f
Andrew Nguyen <andrewnguyen@utexas.edu>
2026-04-06 22:16:42 -0500

    Updated LLVM Paths for work

--- config.fish
+++ config.fish
@@ -1,3 +1,9 @@
+set -gx PATH_TO_LLVM_PROJECT /home/andrew/Projects/llvm-21-1-8 
+set -agx LIBRARY_PATH  /home/andrew/Projects/llvm-21-1-8/install/lib/x86_64-unknown-linux-gnu
+set -agx LIBRARY_PATH  /home/andrew/Projects/llvm-21-1-8/install/lib/nvptx64-nvidia-cuda
+set -agx LD_LIBRARY_PATH /home/andrew/Projects/llvm-21-1-8/install/lib/x86_64-unknown-linux-gnu
+set -agx LD_LIBRARY_PATH /home/andrew/Projects/llvm-21-1-8/install/lib/nvptx64-nvidia-cuda
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_cursor_default block
@@ -16,4 +22,10 @@ if status is-interactive
     end
 
     set -g EDITOR "nvim"
+    
+
+    if test -f "$(pwd)/.venv/bin/activate.fish"
+        echo "Try to activate python venv in: $(pwd)"
+        source "$(pwd)/.venv/bin/activate.fish"
+    end
 end
